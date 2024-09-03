ea6b813aa1c58afa4d022817c529324b591b9a63
Xfactor9390 <78318055+Xfactor9390@users.noreply.github.com>
2024-09-03 16:07:46 +0530

    added index.ts to the root dir

---
+++ createApp.ts
@@ -0,0 +1,36 @@
+import express, { Request,Response } from "express";
+import router from '../server/src/router'
+import passport from "passport";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import cors from 'cors'
+const username = process.env.db_username;
+const password = process.env.db_password;
+const app = express();
+
+export  function createApp() {
+    const app = express();
+
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+app.use(express.json());
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, maxAge: 60000 * 60 * 24 },
+    store:  MongoStore.create({
+        mongoUrl:`mongodb+srv://${username}:${password}@abhinay.yst3wid.mongodb.net/?retryWrites=true&w=majority&appName=abhinay`
+    })
+}))
+app.use(passport.initialize());
+app.use(passport.session())
+    app.use(router);
+    return app;
+
+    
+}
---
+++ index.ts
@@ -0,0 +1,10 @@
+import dbConnect from "../server/src/dbConnection/dbConnection";
+import { createApp } from "../server/createApp";
+const PORT = 3000;
+const app = createApp();
+async function startServer() {
+    await dbConnect();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+}
+startServer();
+export default app;

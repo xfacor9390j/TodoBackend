4c5ed508da503d3e5ae7165abc4276b2a5a067b1
Xfactor9390 <78318055+Xfactor9390@users.noreply.github.com>
2024-09-04 16:40:30 +0530

    finally

--- createApp.ts
+++ createApp.ts
@@ -1,36 +1,46 @@
-import express, { Request,Response } from "express";
-import router from './src/router/index'
+import express, { Request, Response } from "express";
+import router from "./src/router/index";
 import passport from "passport";
 import session from "express-session";
 import MongoStore from "connect-mongo";
-import cors from 'cors'
+import cors from "cors";
 const username = process.env.db_username;
 const password = process.env.db_password;
-const app = express();
-
-export  function createApp() {
-    const app = express();
 
 
-app.use(cors({
-    origin: "https://todo-frontend-theta-one.vercel.app/",
-    credentials: true
-}))
-app.use(express.json());
+export function createApp() {
+  const app = express();
 
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: true, maxAge: 60000 * 60 * 24, sameSite: 'none' },
-    store:  MongoStore.create({
-        mongoUrl:`mongodb+srv://${username}:${password}@abhinay.yst3wid.mongodb.net/?retryWrites=true&w=majority&appName=abhinay`
+  app.use(
+    cors({
+      origin: "https://todo-frontend-theta-one.vercel.app",
+      credentials: true,
     })
-}))
-app.use(passport.initialize());
-app.use(passport.session())
-    app.use(router);
-    return app;
-
-    
+  );
+  console.log("removed the createApp in src folder")
+  app.use(express.json());
+  app.options("*", cors());
+  app.use(
+    session({
+      secret: "keyboard cat",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        path: "/",
+        secure: true,
+        maxAge: 60000 * 60 * 24,
+        httpOnly: true,
+        sameSite: "none",
+        
+       
+      },
+      store: MongoStore.create({
+        mongoUrl: `mongodb+srv://${username}:${password}@abhinay.yst3wid.mongodb.net/?retryWrites=true&w=majority&appName=abhinay`,
+      }),
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(router);
+  return app;
 }

d04b67b89c057ed97f89cdf78d07d07e9f6c2388
Xfactor9390 <78318055+Xfactor9390@users.noreply.github.com>
2024-09-05 17:30:40 +0530

    removed the custom cors middleware

--- createApp.ts
+++ createApp.ts
@@ -13,13 +13,13 @@ const password = process.env.db_password;
 
 export function createApp() {
   const app = express();
-  app.use(function (req:Request, res:Response, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
-      next();
-    });
+  // app.use(function (req:Request, res:Response, next) {
+  //   //Enabling CORS
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
+  //     next();
+  //   });
   app.use(
     cors({
       origin: "https://todo-frontend-theta-one.vercel.app",
